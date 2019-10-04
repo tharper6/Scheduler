118f3feae71f0bba7af61c240810863f3bb97a76
przekwas <luke@covalence.io>
2019-10-04 09:51:26 -0500

    adding new express middlewares

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,20 +1,28 @@
 import * as express from 'express';
 import * as path from 'path';
-import apiRouter from './routes';
 import * as passport from 'passport';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+import * as morgan from 'morgan';
+import * as compression from 'compression';
+import routes from './routes';
 
 import './middleware/localstrategy';
 import './middleware/bearerstrategy';
 
 const app = express();
 
-app.use(passport.initialize())
+app.use(helmet());
+app.use(compression());
+app.use(cors());
+app.use(passport.initialize());
 app.use(express.static('public'));
 app.use(express.json());
-app.use(apiRouter);
+app.use(morgan('dev'));
+app.use(routes);
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+	res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
